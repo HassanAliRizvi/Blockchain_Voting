33f4409124df9800c4add610cc25844ed056638d
HassanAliRizvi <syedriz786@DESKTOP-148LCN0.localdomain>
2020-07-09 14:29:18 -0400

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.16;
+
+contract Election{
+    //Read candidate
+    string public candidate; //declared a variable
+    // Constructor
+    constructor () public{
+        candidate = "Candidate 1";
+    }
+}
